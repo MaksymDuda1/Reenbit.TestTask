815f1adbfba92ce47052640553c3125e05639792
Maksym Duda <maksimduda387@gmail.com>
2024-07-30 11:09:32 +0300

    Changed the text on the home page

--- web-layer/dist/web-layer/server/main.server.mjs
+++ web-layer/dist/web-layer/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-RHJYCFSS.mjs";import"./chunk-KRIETHXV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-7DETRTTF.mjs";import"./chunk-KRIETHXV.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
